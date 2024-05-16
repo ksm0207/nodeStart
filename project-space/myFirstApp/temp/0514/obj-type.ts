d459180569342f9fc3134659546544316d6c192f
Kimsungmin <ksm03071@naver.com>
2024-05-16 22:12:58 +0900

    - node & Typescript / 옵셔널 프로퍼티 / 유니언 타입 표기법
    -

--- project-space/myFirstApp/temp/0514/obj-type.ts
+++ project-space/myFirstApp/temp/0514/obj-type.ts
@@ -9,3 +9,26 @@ function plantStore(items : {name : string , price : number}) {
 
 let result = plantStore({name:"블랙벨벳",price:5500})
 console.info("Result",result)
+
+// 유니언 타입표기법
+/*
+    Type은 서로 다른 두개 이상의 타입들을 사용될 수 있다.
+*/
+
+// 주문번호가 Int 혹은 Str로 넘어온다고 가정해볼까..?@
+
+const orderSeq = (orderNo : number | string) => {
+    if (typeof orderNo === "number") {
+        console.info("Data type of : ", typeof orderNo)
+    }else {
+        console.info("Data type of : ", typeof orderNo)
+    }
+}
+// call Function   : Number
+orderSeq(100)
+// call function 2 : String
+orderSeq("100")
+
+// Test --> orderNo : number | boolean
+// run --> TypeError : "100" is not assignable to parameter error!
+
---
+++ project-space/myFirstApp/temp/0514/optional-type.ts
@@ -0,0 +1,23 @@
+// 옵셔널 프로퍼티.
+// 프로퍼티의 타입을 선택적인 타입으로 지정할 수 있다.
+
+// 아래코드는 상품명 , 종류는 필수로 들어와야한다 그러나 가격은 필수가 아니므로
+// Price에 데이터가 있어도되고 없어도 아래 함수를 사용할 수 있다.
+const products = (items : {productName : string , type : string , price : number, returnYn?: boolean}) =>{
+    console.info("Items : " , items)
+    /*
+    // 일단 에러발생시켜보기 (1)
+    // javascript 코드 실행 시 왜 Typescript를 쓰면좋은지 조금은알것같다.. 
+    if(items.price === undefined) {
+        console.log("Items price is undefined", items.price)
+    }else {
+        console.log("No No price is not undefined", items.price)
+    }
+    */
+}
+
+// 상품정보 / 상품종류 / 상품가격만 넘길경우
+products({productName : "베이비로션" , type : "Lotion", price : 50000,returnYn : false})
+
+// 상품정보 / 상품종류만 넘길경우 - Typescript 에러임 / Javascript 실행됨
+// 일단 에러발생시켜보기 (1) products({productName : "베이비로션" , type : "Lotion"})
